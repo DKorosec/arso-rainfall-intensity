f71bcb261eb3097101508dec507102f30e0b7a4b
Dominik Korosec <dominik.korosec@inova.si>
2020-06-28 23:36:36 +0200

    changed api url to meteo.arso.gov.si because its more stable

--- src/arso-http.ts
+++ src/arso-http.ts
@@ -9,14 +9,14 @@ class RadarDataSnapshot {
     timeCompact: string;
     radarImageUrlPath: string;
     mode: string;
-    
+
     constructor(arsoResultItem: ILatestRadarSnapshotsTimelineHttpResult) {
         this.height = Number(arsoResultItem.height);
         this.width = Number(arsoResultItem.width);
         this.dateTime = new Date(arsoResultItem.valid);
         this.dateCompact = arsoResultItem.date;
         this.timeCompact = arsoResultItem.hhmm;
-        this.radarImageUrlPath = `http://www.vreme.si${arsoResultItem.path}`;
+        this.radarImageUrlPath = `http://meteo.arso.gov.si${arsoResultItem.path}`;
         this.mode = arsoResultItem.mode;
     }
 
@@ -29,7 +29,7 @@ class RadarDataSnapshot {
 }
 
 export async function fetchLatestRadarSnapshotsTimeline(): Promise<RadarDataSnapshot[]> {
-    const apiUrl = 'http://www.vreme.si/api/1.0/inca_precip_data/';
+    const apiUrl = 'http://meteo.arso.gov.si/uploads/probase/www/nowcast/inca/inca_si0zm_data.json?prod=si0zm';
     const response = await axios.get<ILatestRadarSnapshotsTimelineHttpResult[]>(apiUrl, {
         responseType: 'json'
     });
